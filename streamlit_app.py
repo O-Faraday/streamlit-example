b68a5090522747358ec5da14147d9177db86932e
O-Faraday <150429569+O-Faraday@users.noreply.github.com>
2025-01-13 17:04:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,11 +74,20 @@ def run_generation(api_key):
             st.session_state.interview_clicked = True
 
             # Prompt pour générer les questions
-            prompt_recruiter = f"""
-                As an expert recruiter, your job is to prepare an interview for the job described below.
-                Prepare 10 questions to assess the candidate's suitability for the role:
-                {fiche_de_poste}
-            """
+            prompt_recruiter = (
+                f"""As an expert recruiter, your job is to prepare a interview for the job describe in the following paragraph.\n
+                You must prepare a bunch of at max 10 questions to measure the suitability of the candidate. \n\n
+                {fiche_de_poste}\n\n
+                As in the following sample  : \n
+                Based on the job description provided for the Data Engineer position at Scania, here are ten interview questions designed to assess the candidate's suitability for the role:
+
+                1. **Experience with PowerBI and Cloud Technologies:**
+                - Could you walk us through your experience with PowerBI, both on the Cloud and on Report Server? How have you used PowerBI to deliver insights and what challenges have you faced?
+
+                2. **Data Analytics Solution Development:**
+                - Describe a project where you developed data analytics solutions through semantic model building. What was your approach, and what were the outcomes?"
+                """
+            )
             if not st.session_state.questions_generated:
                 response_text = model_answer(client=client, prompt=prompt_recruiter)
                 questions = re.findall(r"- (.*?)\n", response_text, re.DOTALL)
@@ -87,32 +96,95 @@ def run_generation(api_key):
 
             # Afficher les questions et champs de saisie
             st.subheader("Answer the following questions:")
-            for i, question in enumerate(st.session_state.questions_list):
+            # INTERVUEW
+            resume_answers = []
+            candidate_answers = []
+            for i, question in enumerate(st.questions_list):
                 st.write(f"**Question {i + 1}:** {question}")
-                candidate_answer = st.text_input(f"Your answer to question {i + 1}:", key=f"candidate_answer_{i}")
-                st.session_state.resume_answers.append(f"Response extracted for question {i + 1}")
-                st.session_state.candidate_answers.append(candidate_answer)
+                question_prompt = f"""
+                    You are an interviewer and recruitment expert analyzing a candidate's resume to answer the following question:
+                    {question}
+
+                    Refer to the provided candidate resume: {resume}.
+
+                    1- Extract relevant information from the resume to formulate a complete response to the question.
+                    Example:
+                    Question: "How long have you been teaching physics? To which level?"
+                    Resume Excerpt: "Physics Associate Professor  (09/2003 – 08/2016)"
+                    Response: "**RESPONSE:** I have been teaching physics for 13 years."
+
+                    2- If the resume lacks sufficient details to answer fully, identify the missing elements and frame a follow-up question for the candidate.
+                    Example:
+                    Question: "**NEW_QUESTION:** You have been teaching physics to which level?"
+
+                    Your goal is to extract precise answers and draft additional questions to clarify incomplete information.
+                    """
+
+                response, question = extract_response_and_question(model_answer(client=client, prompt=question_prompt))
+    
+                candidate_answer = st.text_input(label=question, key=f"key_{i}")
+                resume_answers.append(response)
+                candidate_answers.append(candidate_answer)
 
         # Bouton "Generate Cover Letter"
         if st.session_state.interview_clicked and st.button("Generate Cover Letter") or st.session_state.analyse_clicked:
             st.session_state.analyse_clicked = True
-            transcription = "\n".join(
-                f"QUESTION: {q}\nRESPONSE: {ra}\nANSWER: {ca}\n"
-                for q, ra, ca in zip(
-                    st.session_state.questions_list, 
-                    st.session_state.resume_answers, 
-                    st.session_state.candidate_answers
-                )
-            )
+            transcription = ""
+            for q, r_a, c_a in zip(questions_list, resume_answers, candidate_answers) :
+                transcription += f"""QUESTION: {q}\nRESPONSE: {r_a}\n{c_a}\n\n"""
+                        
+            # Generation des ongletts
+            tab1, tab2, tab3 = st.tabs(["Cover letter", "Evaluate candidate", "Evalute questions"])
 
-            # Génération de la lettre de motivation
-            cover_letter_prompt = f"""
-                Based on the following interview transcription and the candidate's resume, write a compelling cover letter:
-                {transcription}
-                Resume: {resume}
-            """
-            st.header("Generated Cover Letter")
-            st.write(model_answer(client=client, prompt=cover_letter_prompt))
+            with tab1:
+                st.header("Generate cover letter")
+                cover_letter_prompt = f"""
+                        As a senior candidate, imagine an interview where the recruiter asks the following questions, along with your corresponding answers:  
+                        {transcription}  
+
+                        Additionally, you have the following resume:  
+                        {resume}  
+
+                        Based on this information, write a compelling and concise cover letter that highlights your suitability for the position.  
+                        The cover letter should:  
+                        - Clearly demonstrate how your skills and experiences align with the job requirements.  
+                        - Be impactful and engaging, avoiding generic or overused phrases.  
+                        - Showcase your unique value and enthusiasm for the role.  
+
+                        Focus on creating a document that will capture the recruiter's attention and set you apart as the ideal candidate.
+                        """
+                st.write(model_answer(client=client, prompt=cover_letter_prompt))
+            with tab2:
+                st.header("Evaluate candidate")
+                candidate_evaluation_prompt = f"""
+                        As a senior recruiter, imagine an interview where the recruiter asks the following questions, along with thes corresponding answers:  
+                        {transcription}  
+
+                        Additionally, you have the following job description :  
+                        {fiche_de_poste}  
+
+                        Based on this information, how would you evaluate the candidate :  
+                        - Give a numerical evaluation the candidate.  
+                        - Pro and cons of the candidacy. 
+                        - Do you think there's a need for a second interview ?
+
+                """
+                st.write(model_answer(client=client, prompt=candidate_evaluation_prompt))
+            with tab3:
+                st.header("Evaluate questions")
+                questions_evaluation_prompt = f"""
+                    As a senior recruiter, imagine an interview where the recruiter asks the following questions :  
+                    {transcription}  
+
+                    Additionally, you have the following job description :  
+                    {fiche_de_poste}  
+
+                    Based on this information, how would you evaluate the questions :  
+                     - Give a numerical evaluation the set of questions.  
+                     - How would you improve it?
+
+                    """
+                st.write(model_answer(client=client, prompt=questions_evaluation_prompt))
 
 # Vérifier si la clé API est fournie
 if api_key:
